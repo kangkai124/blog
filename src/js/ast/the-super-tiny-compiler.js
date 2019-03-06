fbec98639c7a0f3b7765ce48704dfaac5ce6201b
kkstrive <kkstrive0124@gmail.com>
2019-03-06 19:08:53 +0800

    learn ast

---
+++ src/js/ast/the-super-tiny-compiler.js
@@ -0,0 +1,448 @@
+/**
+ * TTTTTTTTTTTTTTTTTTTTTTTHHHHHHHHH     HHHHHHHHHEEEEEEEEEEEEEEEEEEEEEE
+ * T:::::::::::::::::::::TH:::::::H     H:::::::HE::::::::::::::::::::E
+ * T:::::::::::::::::::::TH:::::::H     H:::::::HE::::::::::::::::::::E
+ * T:::::TT:::::::TT:::::THH::::::H     H::::::HHEE::::::EEEEEEEEE::::E
+ * TTTTTT  T:::::T  TTTTTT  H:::::H     H:::::H    E:::::E       EEEEEE
+ *         T:::::T          H:::::H     H:::::H    E:::::E
+ *         T:::::T          H::::::HHHHH::::::H    E::::::EEEEEEEEEE
+ *         T:::::T          H:::::::::::::::::H    E:::::::::::::::E
+ *         T:::::T          H:::::::::::::::::H    E:::::::::::::::E
+ *         T:::::T          H::::::HHHHH::::::H    E::::::EEEEEEEEEE
+ *         T:::::T          H:::::H     H:::::H    E:::::E
+ *         T:::::T          H:::::H     H:::::H    E:::::E       EEEEEE
+ *       TT:::::::TT      HH::::::H     H::::::HHEE::::::EEEEEEEE:::::E
+ *       T:::::::::T      H:::::::H     H:::::::HE::::::::::::::::::::E
+ *       T:::::::::T      H:::::::H     H:::::::HE::::::::::::::::::::E
+ *       TTTTTTTTTTT      HHHHHHHHH     HHHHHHHHHEEEEEEEEEEEEEEEEEEEEEE
+ *
+ *    SSSSSSSSSSSSSSS UUUUUUUU     UUUUUUUUPPPPPPPPPPPPPPPPP   EEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRR
+ *  SS:::::::::::::::SU::::::U     U::::::UP::::::::::::::::P  E::::::::::::::::::::ER::::::::::::::::R
+ * S:::::SSSSSS::::::SU::::::U     U::::::UP::::::PPPPPP:::::P E::::::::::::::::::::ER::::::RRRRRR:::::R
+ * S:::::S     SSSSSSSUU:::::U     U:::::UUPP:::::P     P:::::PEE::::::EEEEEEEEE::::ERR:::::R     R:::::R
+ * S:::::S             U:::::U     U:::::U   P::::P     P:::::P  E:::::E       EEEEEE  R::::R     R:::::R
+ * S:::::S             U:::::U     U:::::U   P::::P     P:::::P  E:::::E               R::::R     R:::::R
+ *  S::::SSSS          U:::::U     U:::::U   P::::PPPPPP:::::P   E::::::EEEEEEEEEE     R::::RRRRRR:::::R
+ *   SS::::::SSSSS     U:::::U     U:::::U   P:::::::::::::PP    E:::::::::::::::E     R:::::::::::::RR
+ *     SSS::::::::SS   U:::::U     U:::::U   P::::PPPPPPPPP      E:::::::::::::::E     R::::RRRRRR:::::R
+ *        SSSSSS::::S  U:::::U     U:::::U   P::::P              E::::::EEEEEEEEEE     R::::R     R:::::R
+ *             S:::::S U:::::U     U:::::U   P::::P              E:::::E               R::::R     R:::::R
+ *             S:::::S U::::::U   U::::::U   P::::P              E:::::E       EEEEEE  R::::R     R:::::R
+ * SSSSSSS     S:::::S U:::::::UUU:::::::U PP::::::PP          EE::::::EEEEEEEE:::::ERR:::::R     R:::::R
+ * S::::::SSSSSS:::::S  UU:::::::::::::UU  P::::::::P          E::::::::::::::::::::ER::::::R     R:::::R
+ * S:::::::::::::::SS     UU:::::::::UU    P::::::::P          E::::::::::::::::::::ER::::::R     R:::::R
+ *  SSSSSSSSSSSSSSS         UUUUUUUUU      PPPPPPPPPP          EEEEEEEEEEEEEEEEEEEEEERRRRRRRR     RRRRRRR
+ *
+ * TTTTTTTTTTTTTTTTTTTTTTTIIIIIIIIIINNNNNNNN        NNNNNNNNYYYYYYY       YYYYYYY
+ * T:::::::::::::::::::::TI::::::::IN:::::::N       N::::::NY:::::Y       Y:::::Y
+ * T:::::::::::::::::::::TI::::::::IN::::::::N      N::::::NY:::::Y       Y:::::Y
+ * T:::::TT:::::::TT:::::TII::::::IIN:::::::::N     N::::::NY::::::Y     Y::::::Y
+ * TTTTTT  T:::::T  TTTTTT  I::::I  N::::::::::N    N::::::NYYY:::::Y   Y:::::YYY
+ *         T:::::T          I::::I  N:::::::::::N   N::::::N   Y:::::Y Y:::::Y
+ *         T:::::T          I::::I  N:::::::N::::N  N::::::N    Y:::::Y:::::Y
+ *         T:::::T          I::::I  N::::::N N::::N N::::::N     Y:::::::::Y
+ *         T:::::T          I::::I  N::::::N  N::::N:::::::N      Y:::::::Y
+ *         T:::::T          I::::I  N::::::N   N:::::::::::N       Y:::::Y
+ *         T:::::T          I::::I  N::::::N    N::::::::::N       Y:::::Y
+ *         T:::::T          I::::I  N::::::N     N:::::::::N       Y:::::Y
+ *       TT:::::::TT      II::::::IIN::::::N      N::::::::N       Y:::::Y
+ *       T:::::::::T      I::::::::IN::::::N       N:::::::N    YYYY:::::YYYY
+ *       T:::::::::T      I::::::::IN::::::N        N::::::N    Y:::::::::::Y
+ *       TTTTTTTTTTT      IIIIIIIIIINNNNNNNN         NNNNNNN    YYYYYYYYYYYYY
+ *
+ *         CCCCCCCCCCCCC     OOOOOOOOO     MMMMMMMM               MMMMMMMMPPPPPPPPPPPPPPPPP   IIIIIIIIIILLLLLLLLLLL             EEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRR
+ *      CCC::::::::::::C   OO:::::::::OO   M:::::::M             M:::::::MP::::::::::::::::P  I::::::::IL:::::::::L             E::::::::::::::::::::ER::::::::::::::::R
+ *    CC:::::::::::::::C OO:::::::::::::OO M::::::::M           M::::::::MP::::::PPPPPP:::::P I::::::::IL:::::::::L             E::::::::::::::::::::ER::::::RRRRRR:::::R
+ *   C:::::CCCCCCCC::::CO:::::::OOO:::::::OM:::::::::M         M:::::::::MPP:::::P     P:::::PII::::::IILL:::::::LL             EE::::::EEEEEEEEE::::ERR:::::R     R:::::R
+ *  C:::::C       CCCCCCO::::::O   O::::::OM::::::::::M       M::::::::::M  P::::P     P:::::P  I::::I    L:::::L                 E:::::E       EEEEEE  R::::R     R:::::R
+ * C:::::C              O:::::O     O:::::OM:::::::::::M     M:::::::::::M  P::::P     P:::::P  I::::I    L:::::L                 E:::::E               R::::R     R:::::R
+ * C:::::C              O:::::O     O:::::OM:::::::M::::M   M::::M:::::::M  P::::PPPPPP:::::P   I::::I    L:::::L                 E::::::EEEEEEEEEE     R::::RRRRRR:::::R
+ * C:::::C              O:::::O     O:::::OM::::::M M::::M M::::M M::::::M  P:::::::::::::PP    I::::I    L:::::L                 E:::::::::::::::E     R:::::::::::::RR
+ * C:::::C              O:::::O     O:::::OM::::::M  M::::M::::M  M::::::M  P::::PPPPPPPPP      I::::I    L:::::L                 E:::::::::::::::E     R::::RRRRRR:::::R
+ * C:::::C              O:::::O     O:::::OM::::::M   M:::::::M   M::::::M  P::::P              I::::I    L:::::L                 E::::::EEEEEEEEEE     R::::R     R:::::R
+ * C:::::C              O:::::O     O:::::OM::::::M    M:::::M    M::::::M  P::::P              I::::I    L:::::L                 E:::::E               R::::R     R:::::R
+ *  C:::::C       CCCCCCO::::::O   O::::::OM::::::M     MMMMM     M::::::M  P::::P              I::::I    L:::::L         LLLLLL  E:::::E       EEEEEE  R::::R     R:::::R
+ *   C:::::CCCCCCCC::::CO:::::::OOO:::::::OM::::::M               M::::::MPP::::::PP          II::::::IILL:::::::LLLLLLLLL:::::LEE::::::EEEEEEEE:::::ERR:::::R     R:::::R
+ *    CC:::::::::::::::C OO:::::::::::::OO M::::::M               M::::::MP::::::::P          I::::::::IL::::::::::::::::::::::LE::::::::::::::::::::ER::::::R     R:::::R
+ *      CCC::::::::::::C   OO:::::::::OO   M::::::M               M::::::MP::::::::P          I::::::::IL::::::::::::::::::::::LE::::::::::::::::::::ER::::::R     R:::::R
+ *         CCCCCCCCCCCCC     OOOOOOOOO     MMMMMMMM               MMMMMMMMPPPPPPPPPP          IIIIIIIIIILLLLLLLLLLLLLLLLLLLLLLLLEEEEEEEEEEEEEEEEEEEEEERRRRRRRR     RRRRRRR
+ *
+ * =======================================================================================================================================================================
+ * =======================================================================================================================================================================
+ * =======================================================================================================================================================================
+ * =======================================================================================================================================================================
+ */
+
+/**
+ * Thanks to Jamie https://github.com/jamiebuilds.
+ */
+
+/**
+ * Gona to compile lisp-like function calls into C-like
+ * function callls.
+ *
+ *                  LISP                      C
+ *
+ *   2 + 2          (add 2 2)                 add(2, 2)
+ *   4 - 2          (subtract 4 2)            subtract(4, 2)
+ *   2 + (4 - 2)    (add 2 (subtract 4 2))    add(2, subtract(4, 2))
+ */
+
+/**
+* ============================================================================
+*                                   (/^▽^)/
+*                                THE TOKENIZER!
+* ============================================================================
+*/
+
+/**
+ *
+ * @param {String} input
+ * @return {Array}
+ *
+ */
+function tokenizer (input) {
+  let current = 0
+  let tokens = []
+
+  while (current < input.length) {
+    let char = input[current]
+
+    if (char === '(') {
+      tokens.push({
+        type: 'paren',
+        value: '('
+      })
+
+      current++
+      continue
+    }
+
+    if (char === ')') {
+      tokens.push({
+        type: 'paren',
+        value: ')'
+      })
+
+      current++
+      continue
+    }
+
+    let WHITESPACE = /\s/
+    if (WHITESPACE.test(char)) {
+      current++
+      continue
+    }
+
+    let NUMBERS = /[0-9]/
+    if (NUMBERS.test(char)) {
+      let value = ''
+      while (NUMBERS.test(char)) {
+        value += char
+        char = input[++current]
+      }
+
+      tokens.push({ type: 'number', value })
+      continue
+    }
+
+    if (char === '"') {
+      let value = ''
+      char = input[++current]
+
+      while (char !== '"') {
+        value += char
+        char = input[++current]
+      }
+
+      char = input[++current]
+      tokens.push({ type: 'string', value })
+      continue
+    }
+
+    let LETTERS = /[a-z]/i
+    if (LETTERS.test(char)) {
+      let value = ''
+
+      while (LETTERS.test(char)) {
+        value += char
+        char = input[++current]
+      }
+
+      tokens.push({ type: 'name', value })
+      continue
+    }
+
+    throw new TypeError('I dont know what this character is: ' + char)
+  }
+
+  return tokens
+}
+
+/**
+ * ============================================================================
+ *                                 ヽ/❀o ل͜ o\ﾉ
+ *                                THE PARSER!!!
+ * ============================================================================
+ */
+
+/**
+  *
+  * @param {Array} tokens
+  * @return {Object}
+  *
+  */
+
+function parser (tokens) {
+  let current = 0
+
+  function walk () {
+    let token = tokens[current]
+
+    if (token.type === 'number') {
+      current++
+      return {
+        type: 'NumberLiteral',
+        value: token.value
+      }
+    }
+
+    if (token.type === 'string') {
+      current++
+      return {
+        type: 'StringLiteral',
+        value: token.value
+      }
+    }
+
+    if (token.type === 'paren' && token.value === '(') {
+      token = tokens[++current]
+      let node = {
+        type: 'CallExpression',
+        name: token.value,
+        params: []
+      }
+
+      token = tokens[++current]
+
+      while (
+        (token.type !== 'paren') ||
+        (token.type === 'paren' && token.value !== ')')
+      ) {
+        node.params.push(walk())
+        token = tokens[current]
+      }
+
+      current++
+
+      return node
+    }
+
+    throw new TypeError(token.type)
+  }
+
+  let ast = {
+    type: 'Program',
+    body: []
+  }
+
+  while (current < tokens.length) {
+    ast.body.push(walk())
+  }
+
+  return ast
+}
+
+/**
+ * ============================================================================
+ *                                 ⌒(❀>◞౪◟<❀)⌒
+ *                               THE TRAVERSER!!!
+ * ============================================================================
+ */
+
+/**
+  *
+  * @param {Object} ast
+  * @param {*} visitor
+  *
+  */
+function traverser (ast, visitor) {
+  function traverseArray (array, parent) {
+    array.forEach(a => {
+      traverseNode(a, parent)
+    })
+  }
+
+  function traverseNode (node, parent) {
+    let methods = visitor[node.type]
+
+    if (methods && methods.enter) {
+      methods.enter(node, parent)
+    }
+
+    switch (node.type) {
+    case 'Program':
+      traverseArray(node.body, node)
+      break
+
+    case 'CallExpression':
+      traverseArray(node.params, node)
+      break
+
+    case 'NumberLiteral':
+    case 'StringLiteral':
+      break
+    default:
+      throw new TypeError(node.type)
+    }
+
+    if (methods && methods.exit) {
+      methods.exit(node, parent)
+    }
+  }
+
+  traverseNode(ast, null)
+}
+
+/**
+ * ============================================================================
+ *                                   ⁽(◍˃̵͈̑ᴗ˂̵͈̑)⁽
+ *                              THE TRANSFORMER!!!
+ * ============================================================================
+ */
+
+/**
+  *
+  * @param {Object} ast
+  * @return {Object}
+  *
+  */
+function transformer (ast) {
+  let newAst = {
+    type: 'Program',
+    body: []
+  }
+
+  ast._context = newAst.body
+
+  traverser(ast, {
+    NumberLiteral: {
+      enter (node, parent) {
+        parent._context.push({
+          type: 'NumberLiteral',
+          value: node.value
+        })
+      }
+    },
+
+    StringLiteral: {
+      enter (node, parent) {
+        parent._context.push({
+          type: 'StringLiteral',
+          value: node.value
+        })
+      }
+    },
+
+    CallExpression: {
+      enter (node, parent) {
+        let expression = {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: node.name
+          },
+          arguments: []
+        }
+
+        node._context = expression.arguments
+        if (parent.type !== 'CallExpression') {
+          expression = {
+            type: 'ExpressionStatement',
+            expression: expression
+          }
+        }
+
+        parent._context.push(expression)
+      }
+    }
+  })
+
+  return newAst
+}
+
+/**
+ * ============================================================================
+ *                               ヾ（〃＾∇＾）ﾉ♪
+ *                            THE CODE GENERATOR!!!!
+ * ============================================================================
+ */
+
+/**
+  *
+  * @param {Object} node
+  * @return {String}
+  *
+  */
+function codeGenerator (node) {
+  switch (node.type) {
+  case 'Program':
+    return node.body.map(codeGenerator).join('\n')
+
+  case 'ExpressionStatement':
+    return (
+      codeGenerator(node.expression) + ';'
+    )
+
+  case 'CallExpression':
+    return (
+      codeGenerator(node.callee) +
+        '(' +
+        node.arguments.map(codeGenerator).join(', ') +
+        ')'
+    )
+
+  case 'Identifier':
+    return node.name
+
+  case 'NumberLiteral':
+    return node.value
+
+  case 'StringLiteral':
+    return '"' + node.value + '"'
+
+  default:
+    throw new TypeError(node.type)
+  }
+}
+
+/**
+ * ============================================================================
+ *                                  (۶* ‘ヮ’)۶”
+ *                         !!!!!!!!THE COMPILER!!!!!!!!
+ * ============================================================================
+ */
+
+/**
+  *
+  * @param {String} input
+  * @return {String}
+  *
+  */
+function compiler (input) {
+  let tokens = tokenizer(input)
+  let ast = parser(tokens)
+  let newAst = transformer(ast)
+  let output = codeGenerator(newAst)
+
+  return output
+}
+
+module.exports = {
+  tokenizer,
+  parser,
+  traverser,
+  transformer,
+  codeGenerator,
+  compiler
+}
